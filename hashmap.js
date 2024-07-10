12fa2eadb6e07d174b871783a6df835ca6744630
Louis Escoffier <louis.escoffier.bagrel@gmail.com>
2024-07-10 14:45:00 +0200

    Imports linkedList code

---
+++ hashmap.js
@@ -0,0 +1,18 @@
+import LinkedList from "./linkedList";
+
+export default class HashMap {
+  constructor() {
+    this.hashTable = new Array(16);
+  }
+
+  hash(key) {
+    let hashCode = 0;
+
+    const primeNumber = 31;
+    for (let i = 0; i < key.length; i++) {
+      hashCode = primeNumber * hashCode + key.charCodeAt(i);
+    }
+
+    return hashCode;
+  }
+}
